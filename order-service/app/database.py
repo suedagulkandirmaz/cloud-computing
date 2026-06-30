8520905ceae4dd0145a762370f569d3636d5ede1
suedagulkandirmaz <skandirmaz@stud.macromedia.de>
2026-06-30 14:11:05 +0200

    change deploy

--- order-service/app/database.py
+++ order-service/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./orders.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
